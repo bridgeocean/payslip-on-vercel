4884078d4dd350fc580af014d825634b1b9aaff3
bridgeocean <149322598+bridgeocean@users.noreply.github.com>
2025-11-01 21:40:04 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    // keep native binaries external so Vercel Lambda can use them as-is
-    serverComponentsExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
-  },
-  webpack: (config) => {
-    if (!config.externals) config.externals = [];
-    // extra safety to avoid bundling chromium
-    config.externals.push('@sparticuz/chromium');
-    return config;
-  },
+  reactStrictMode: true
 };
 export default nextConfig;
